691ba75ab6453a5d6b8247d26305bf805e6bbf4e
falcosan <danielefalche@gmail.com>
2021-03-22 19:28:01 +0100

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,45 @@
+
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'server',
+
   head: {
-    title: 'aprograma_nuxt',
-    htmlAttrs: {
-      lang: 'en',
-    },
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  loading: { color: '#fff' },
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  css: [
+  ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  plugins: [
+    '~/plugins/components'
+  ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
+
     '@nuxtjs/tailwindcss',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    [
+      'storyblok-nuxt',
+      {
+        accessToken: 'Hfa8vm7STr9uvb8iPaDIKQtt',
+        cacheProvider: 'memory'
+      }
+    ],
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend(config, ctx) {
+    }
+  }
 }

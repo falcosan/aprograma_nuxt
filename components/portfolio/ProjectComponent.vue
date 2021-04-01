2d6da6ade92d28aa12620d299a81e09e6276c0fd
falcosan <danielefalche@gmail.com>
2021-04-01 18:27:03 +0200

    .

--- components/portfolio/ProjectComponent.vue
+++ components/portfolio/ProjectComponent.vue
@@ -1,5 +1,11 @@
 <template>
-  <div v-html="getDescription" />
+  <div>
+    <div v-html="getDescription" />
+    <div> {{ blok.title }}</div>
+    <img :src="blok.image.filename" :alt="blok.image.alt">
+    <div> {{ blok.start_date }} {{ blok.end_date }}</div>
+    <a :href="blok.project_url" />
+  </div>
 </template>
 <script>
 import marked from 'marked'

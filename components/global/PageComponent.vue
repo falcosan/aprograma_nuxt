8075183c540384ccf0de2a4cd633a8d79de63009
falcosan <danielefalche@gmail.com>
2021-04-14 10:17:36 +0200

    .

--- components/global/PageComponent.vue
+++ components/global/PageComponent.vue
@@ -5,7 +5,7 @@
     appear-active-class="transition-opacity duration-200 out-in"
     appear-class="opacity-0"
     :class="`${blok.name.toLowerCase()}-page h-full
-      ${$customClass('home', 'h-full-adapted flex items-center')}
+      ${$customClass('index', 'h-full-adapted flex items-center')}
       ${$customClass('about', 'h-full-adapted flex items-center')}
       ${$customClass('portfolio', 'h-full-adapted flex items-center')}
       ${$customClass('contact', 'h-full-adapted grid items-center grid-rows-contact grid-cols-2 gap-x-10')}`"
--- components/global/TextContentComponent.vue
+++ components/global/TextContentComponent.vue
@@ -1,14 +1,14 @@
 <template>
   <div
     :class="`text-section
-    ${$customClass('home', 'items-center flex my-0')}
+    ${$customClass('index', 'items-center flex my-0')}
     ${$customClass('about', 'items-center flex my-0')}
     ${$customClass('contact', 'h-full flex flex-1 m-0 items-center justify-center row-start-2 row- col-start-2 col-end-2 bg-blue-400')}`"
   >
     <span
       key="text-content"
       :class="`text-content mx-auto py-6 px-10
-      ${$customClass('home', 'text-7xl')}
+      ${$customClass('index', 'text-7xl')}
       ${$customClass('about', 'bg-yellow-300')}
       ${$customClass('contact', 'w-full h-full grid text-white')}`"
       v-html="getText"

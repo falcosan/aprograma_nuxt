20edc6081762f69c8b462450cab9d983db79fcc2
falcosan <danielefalche@gmail.com>
2021-04-17 16:56:42 +0200

    .

--- components/global/PageComponent.vue
+++ components/global/PageComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <section
     :class="`${blok.name.toLowerCase()}-page w-8/12 my-0 mx-auto h-screen
-      ${$customClass('home', 'all', 'flex items-center')}
-      ${$customClass('about', 'all', 'flex items-center')}
-      ${$customClass('portfolio', 'all', 'flex items-center')}
-      ${$customClass('contact', 'all', 'grid grid-cols-2 content-center gap-x-10')}
-      ${$customClass('blog', 'all', 'p-6')}`"
+      ${$customClass('home','all', 'flex items-center')}
+      ${$customClass('about','all', 'flex items-center')}
+      ${$customClass('portfolio','all', 'flex items-center')}
+      ${$customClass('contact','all', 'grid grid-cols-2 content-center gap-x-10')}
+      ${$customClass('blog','all', 'p-6')}`"
   >
     <component
       :is="component.component"
--- components/global/TextContentComponent.vue
+++ components/global/TextContentComponent.vue
@@ -3,14 +3,16 @@
     :key="blok._uid"
     :class="`text-container
     ${$customClass('home', 'all', 'flex items-center my-0')}
+    ${$customClass('home', blok.name === 'text_home_2', 'p-10 bg-red-500')}
     ${$customClass('about', 'all', 'flex items-center my-0')}
     ${$customClass('contact', 'all', 'col-start-2 col-end-2 bg-blue-400')}`"
   >
     <span
-      :class="`text-content bg-gray-700mx-auto
+      :style="`color: ${blok.color.color}`"
+      :class="`text-content mx-auto
       ${$customClass('home', 'all', 'flex items-center my-0 whitespace-nowrap')}
       ${$customClass('about', 'all', 'py-6 px-10 bg-yellow-300')}
-      ${$customClass('contact', 'all', 'w-full h-full py-6 px-10 grid text-white')}`"
+      ${$customClass('contact', 'all', 'w-full h-full py-6 px-10 grid')}`"
       v-html="blok.text.content"
     />
   </div>
@@ -21,11 +23,6 @@ export default {
     blok: {
       type: Object,
       required: true
-    }
-  },
-  data () {
-    return {
-      name: this.blok.name
     }
   }
 }

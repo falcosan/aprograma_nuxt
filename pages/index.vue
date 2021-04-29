2b3d2d11e849e5b504b69e590b07cd053d679ddd
falcosan <danielefalche@gmail.com>
2021-04-29 19:40:50 +0200

    develop

--- pages/index.vue
+++ pages/index.vue
@@ -9,10 +9,16 @@
 <script>
 export default {
   asyncData (context) {
+    const slug = (context.route.path === '/' || context.route.path === '') ? 'home' : context.route.path
     return context.app.$storyapi
-      .get(`cdn/stories/${context.store.state.language.language}/home`)
+      .get(`cdn/stories/${context.store.state.language.language}/${slug}`)
       .then((res) => {
         return res.data
+      })
+      .catch((res) => {
+        context.$errorMessage(res.response, `Sorry, but this content: "${res.response.config.url.substring(
+          res.response.config.url.lastIndexOf('/') + 1)}" doesn't exist`, `Sorry, but the content: "${res.response.config.url.substring(
+          res.response.config.url.lastIndexOf('/') + 1)}" has a problem or doesn't exist`)
       })
   },
   watch: {

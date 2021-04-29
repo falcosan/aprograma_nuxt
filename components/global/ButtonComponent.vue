5334a76ff1a6e4c5a2513c2d75555ac800d9cd98
falcosan <danielefalche@gmail.com>
2021-04-29 23:25:45 +0200

    develop

--- components/global/ButtonComponent.vue
+++ components/global/ButtonComponent.vue
@@ -1,12 +1,21 @@
 <template>
-  <input class="button cursor-pointer" :style="`background-color: ${blok.background_color.color}; color: ${blok.text_color.color};`" :type="blok.type" :value="blok.text">
+  <input v-if="blok" class="button cursor-pointer" :style="`background-color: ${blok.background_color.color}; color: ${blok.text_color.color};`" :type="blok.type" :value="blok.text">
+  <input v-else class="button cursor-pointer" :type="type" :value="text">
 </template>
 <script>
 export default {
   props: {
     blok: {
       type: Object,
-      required: true
+      default: undefined
+    },
+    type: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
     }
   }
 }
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="py-10 md:py-6">
-    <h1 v-if="error.statusCode === 404">
-      {{ error.statusCode }}
-      {{ error.message }}
-    </h1>
-    <h1 v-else>
-      {{ error.statusCode }}
-      {{ error.message }}
-    </h1>
+  <div class="max-w-sm sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-7xl my-0 mx-auto py-16 px-6">
+    <div v-if="error.statusCode === 404" class="max-h-screen grid grid-flow-row text-center p-20 bg-gray-200">
+      <span class="text-9xl font-bold">{{ error.statusCode }}</span>
+      <span class="w-max h-max justify-self-center transform rotate-90"> = ( </span>
+      <span class="text-2xl"> {{ error.message }}</span>
+    </div>
+    <div v-else class="max-h-screen grid gap-y-10 grid-flow-row text-center p-20 bg-gray-200">
+      <span class="text-9xl font-bold">{{ error.statusCode }}</span>
+      <span class="w-max h-max justify-self-center transform rotate-90"> = ( </span>
+      <span class="text-2xl"> {{ error.message }}</span>
+      <NuxtLink class=" w-max justify-self-center py-3 px-10 filter grayscale bg-gray-500 text-white" to="/">
+        Home
+      </NuxtLink>
+    </div>
   </div>
 </template>
 

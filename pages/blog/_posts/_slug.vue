ceb87414d8ad75e5978f0692993fb50e3bddf363
falcosan <danielefalche@gmail.com>
2021-04-30 14:49:03 +0200

    develop

--- pages/blog/_posts/_slug.vue
+++ pages/blog/_posts/_slug.vue
@@ -12,6 +12,17 @@ export default {
         return res.data
       })
   },
+  data () {
+    return {
+      story: {
+        content: {}
+      }
+    }
+  },
+  async fetch () {
+    const { data } = await this.$storyapi.get(`cdn/stories${this.$store.state.language.language ? `/${this.$store.state.language.language}` : this.$store.state.language.language}${this.$route.path}`)
+    this.story = data.story
+  },
   watch: {
     '$store.state.language.language': { handler () { this.$nuxt.refresh() } }
   }

030909ef4cfe4edff4d06b7cddedae1bf12b92b3
falcosan <danielefalche@gmail.com>
2021-04-30 11:27:02 +0200

    develop

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -10,13 +10,6 @@
 import Post from '@/components/blog/PostComponent'
 export default {
   components: { Post },
-  asyncData (context) {
-    return context.app.$storyapi
-      .get(`cdn/stories/${context.store.state.language.language}${context.route.path}`)
-      .then((res) => {
-        return res.data
-      })
-  },
   data () {
     return {
       story: {

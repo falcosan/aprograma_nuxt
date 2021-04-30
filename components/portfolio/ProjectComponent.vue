31fa64e27e941e7c7257d1995bd8831f8e3b2431
falcosan <danielefalche@gmail.com>
2021-04-30 15:56:31 +0200

    develop

--- components/portfolio/ProjectComponent.vue
+++ components/portfolio/ProjectComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="project max-w-lg md:max-w-none md:w-9/12 lg:max-w-3xl xl:max-w-4xl 2xl:max-w-7xl grid gap-5 my-0 mx-auto py-16 px-8">
-    <h1 class="project-title flex items-center col-start-1 col-end-4 lg:col-end-3 p-5 text-xl lg:text-2xl break-words">
+    <h1 class="project-title flex justify-center items-center col-start-1 col-end-4 lg:col-end-3 p-5 text-xl lg:text-2xl text-center lg:text-left break-words">
       {{ blok.title }}
     </h1>
     <div v-if="blok.image.filename" class="image-container max-h-full col-start-1 col-end-4 border-2" :style="`border-color: ${blok.project_background_color.color}`">

6343ca9887f7d479549cd095eefb75cb12a78826
falcosan <danielefalche@gmail.com>
2021-05-10 10:59:03 +0200

    develop

--- components/global/PageComponent.vue
+++ components/global/PageComponent.vue
@@ -4,13 +4,14 @@
       {{ blok.title }}
     </h1>
     <div
-      :class="`${blok.name.toLowerCase()}-content grid gap-5 ${rowComponent.length === blok.body.length ? 'grid-flow-col' : ''} ${customClass('index', 'all', 'justify-center')}`"
+      :class="`${blok.name.toLowerCase()}-content grid gap-5 auto-cols-fr ${rowComponent.length === blok.body.length ? 'grid-flow-col' : ''} ${customClass('index', 'all', 'justify-center')}`"
     >
       <component
         :is="component.component"
         v-for="component in blok.body"
         :key="component._uid"
-        :class="component.row_container ? '' : `lg:col-start-1 lg:col-end-${rowComponent.length + 1}`"
+        :style="component.row_container || $store.state.data.windowWidth < 1024 ? false : `grid-column-end: ${rowComponent.length + 1}`"
+        :class="`${blok.name.toLowerCase()}-component ${component.row_container ? '' : 'lg:col-start-1'}`"
         :blok="component"
       />
     </div>

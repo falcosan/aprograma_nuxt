24daf274146e8359281c3d4a55d9f0feac2e130b
falcosan <danielefalche@gmail.com>
2021-05-15 10:30:42 +0200

    .

--- components/global/MediaComponent.vue
+++ components/global/MediaComponent.vue
@@ -1,31 +1,33 @@
 <template>
-  <span class="media-container grid gap-5 w-full">
+  <span class="media-container relative grid gap-5 w-full">
     <img
-      v-if="blok && blok.media.filename && lookFile === 'image'"
-      :width="blok && blok.width ? `${blok.width}${blok.unit}`: blok && blok.width && blok.full_space ? 100 : width"
-      :height="blok && blok.height ? `${blok.height}${blok.unit}` : blok && blok.width && blok.full_space ? 100 : height"
-      :class="`media ${blok.media.filename
+      v-if="(blok && blok.media.filename && lookFile === 'image') || image"
+      :width="blok && blok.width ? `${blok.width}${blok.unit}`: blok && blok.width && blok.full_space ? 100 : width ? width : false"
+      :height="blok && blok.height ? `${blok.height}${blok.unit}` : blok && blok.width && blok.full_space ? 100 : height ? height : false"
+      :class="`media ${blok && blok.media.filename ? blok.media.filename : source
         .split(/[\\/]/)
         .pop()
         .replace(/\.[^/.]+$/, '')}-media media-image my-0 mx-auto object-contain object-center pointer-events-none select-none`"
-      :src="blok.media.filename"
-      :alt="blok.media.alt"
+      :src="blok && blok.media.filename ? blok.media.filename : source"
+      :alt="blok && blok.media.filename ? blok.media.alt : alt ? alt : ''"
       :type="`image/${imageType()}`"
     >
     <video
-      v-else-if="blok && blok.media.filename"
-      :class="`${blok.media.filename
+      v-else-if="(blok && blok.media.filename) || video"
+      :class="`${blok && blok.media.filename ? blok.media.filename : source
         .split(/[\\/]/)
         .pop()
         .replace(/\.[^/.]+$/, '')}-media media-video my-0 mx-auto object-contain object-center pointer-events-none select-none`"
+      :width="blok && blok.width ? `${blok.width}${blok.unit}`: blok && blok.width && blok.full_space ? 100 : width ? width : false"
+      :height="blok && blok.height ? `${blok.height}${blok.unit}` : blok && blok.width && blok.full_space ? 100 : height ? height : false"
       playsinline
       autoplay
       muted
       loop
     >
-      <source :src="blok.media.filename" :type="`video/${blok.media.filename.toLowerCase().split('.').pop()}`">
+      <source :src="blok && blok.media.filename ? blok.media.filename : source" :type="`video/${blok && blok.media.filename ? blok.media.filename.toLowerCase().split('.').pop() : source.toLowerCase().split('.').pop()}`">
     </video>
-    <p v-if="blok && blok.title && blok.show_title" class="media-title text-center">{{ blok.title }}</p>
+    <p v-if="(blok && blok.title && blok.show_title) || title" class="media-title text-center" v-text="blok && blok.title ? blok.title : title" />
   </span>
 </template>
 
@@ -36,6 +38,18 @@ export default {
       type: Object,
       default: undefined
     },
+    title: {
+      type: String,
+      default: ''
+    },
+    source: {
+      type: String,
+      default: ''
+    },
+    alt: {
+      type: String,
+      default: ''
+    },
     width: {
       type: String,
       default: ''
@@ -43,17 +57,36 @@ export default {
     height: {
       type: String,
       default: ''
+    },
+    image: {
+      type: Boolean,
+      default: false
+    },
+    video: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {
     lookFile () {
-      return (/(gif|jpe?g|tiff?|png|webp|bmp)/gi).test(this.blok.media.filename.toLowerCase().split('.').pop()) ? 'image' : 'video'
+      return this.blok && this.blok.media.filename ? (/(gif|jpe?g|tiff?|png|webp|bmp)/gi).test(this.blok.media.filename.toLowerCase().split('.').pop()) ? 'image' : 'video' : false
     }
   },
   methods: {
     imageType () {
-      if (this.blok && this.lookFile === 'image') {
+      if (this.blok && this.blok.media.filename && this.lookFile === 'image') {
         switch (this.blok.media.filename.toLowerCase().split('.').pop()) {
+          case 'jpg':
+            return 'jpeg'
+          case 'png':
+            return 'png'
+          case 'svg':
+            return 'svg+xml'
+          case 'gif':
+            return 'gif'
+        }
+      } else if (this.image) {
+        switch (this.source.toLowerCase().split('.').pop()) {
           case 'jpg':
             return 'jpeg'
           case 'png':

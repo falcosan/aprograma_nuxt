d2368d2ca62bdb37fbec2b087512d44212c20012
falcosan <danielefalche@gmail.com>
2021-05-20 12:21:52 +0200

    develop

--- components/blog/PostTeaserComponent.vue
+++ components/blog/PostTeaserComponent.vue
@@ -11,7 +11,7 @@
       >
         <component
           :is="lookFile()"
-          :class="`teaser-file w-full object-cover object-center select-none ${$parent.blok.row_container ? 'h-full' : 'lg:w-1/2'}`"
+          :class="`teaser-file w-full object-cover object-center select-none ${$parent.blok.row_container ? 'h-full' : 'h-40 ss:h-48 sm:h-56 md:h-72 lg:h-full lg:w-1/2'}`"
           :alt="postContent.file.alt"
           :src="postContent.file.filename"
         />

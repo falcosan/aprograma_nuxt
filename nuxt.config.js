b3dec6372a477b16ad200b763c4bf14ee01f14f9
falcosan <danielefalche@gmail.com>
2021-05-26 20:53:26 +0200

    develop

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
   plugins: [
     '~/plugins/injects',
     '~/plugins/components',
+    '~/plugins/polyfill.client.js',
     '~/plugins/touchScreen.client.js',
     '~/plugins/persistedState.client.js'
   ],

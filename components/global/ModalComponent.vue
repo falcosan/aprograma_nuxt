676ff7f5ad79457f510bdc68dcd8e3fcfcfa1a04
falcosan <danielefalche@gmail.com>
2021-05-28 01:08:35 +0200

    .

--- components/global/ModalComponent.vue
+++ components/global/ModalComponent.vue
@@ -5,7 +5,7 @@
       <div
         v-if="openEvent || open"
         ref="modal"
-        :class="`modal-backdrop fixed flex justify-center inset-0 px-5 pt-10 lg:pt-5 lg:px-12 z-50 overflow-auto ${modalStyle} ${closeMode ? 'cursor-pointer' : ''}`"
+        :class="`modal-backdrop fixed flex justify-center inset-0 px-5 py-10 lg:py-5 lg:px-10 z-50 overflow-auto ${modalStyle} ${closeMode ? 'cursor-pointer' : ''}`"
         tabindex="0"
         @click.self.stop="closeMode ? closeModal() : false"
         @keydown.esc="closeMode ? closeModal() : false"
@@ -88,20 +88,14 @@ export default {
 }
 </script>
 <style>
-.body-container > *{
+.body-container > * {
   max-height: calc(100vh - 5rem);
-  margin: auto 0;
   box-sizing: content-box;
-}
-.body-container > *:last-child{
-  padding-bottom: 2.5rem;
+  margin: auto 0;
 }
 @media screen and (min-width: 1024px) {
-  .body-container > *{
+  .body-container > * {
     max-height: calc(100vh - 2.5rem);
-  }
-  .body-container > *:last-child{
-    padding-bottom: 1.25rem;
   }
 }
 </style>
--- components/layout/LoadingComponent.vue
+++ components/layout/LoadingComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <div v-if="loading" class="loader">
-    <div :class="`bar fixed w-full h-0.5 z-40 ${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'bottom-10' : ''}`">
+    <div :class="`bar-container fixed w-full h-0.5 z-40 ${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'bottom-10' : ''}`">
       <div class="loader-bar absolute h-full filter grayscale bg-gray-800" />
     </div>
     <div :class="`fixed z-50 overflow-hidden bg-white ${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'top-0 right-1/2 p-1 transform translate-x-1/2 rounded-b-full' : 'h-24 w-24'}`">
       <Logo
-        class="animate-spin opacity-75 filter contrast-0 brightness-50"
+        :class="`logo-loading animate-spin filter opacity-50 contrast-0 brightness-50`"
         :size="`${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'w-12' : 'w-full'}`"
       />
     </div>

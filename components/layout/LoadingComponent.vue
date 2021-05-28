d4cf3c18ce5b3b71d3f85ecabc2b47031fba2423
falcosan <danielefalche@gmail.com>
2021-05-28 18:08:09 +0200

    develop

--- components/layout/LoadingComponent.vue
+++ components/layout/LoadingComponent.vue
@@ -6,7 +6,7 @@
     <transition
       enter-active-class="duration-300 in-out"
       enter-class="opacity-0"
-      leave-active-class="duration-1000 out-in"
+      leave-active-class="duration-700 out-in"
       leave-to-class="opacity-0"
     >
       <div v-show="loadingLogo" :class="`loader-logo fixed z-50 overflow-hidden bg-white ${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'top-0 right-1/2 p-1 transform translate-x-1/2 rounded-b-full' : 'h-24 w-24'}`">

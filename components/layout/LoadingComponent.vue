db1d32b5a241d4ed0f05d0308f53c7951eff2a7b
falcosan <danielefalche@gmail.com>
2021-05-28 10:43:17 +0200

    .

--- components/layout/LoadingComponent.vue
+++ components/layout/LoadingComponent.vue
@@ -1,18 +1,17 @@
 <template>
   <div v-if="loading" class="loader">
-    <div :class="`bar-container fixed w-full h-0.5 z-40 ${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'bottom-10' : ''}`">
+    <div v-show="loadingBar" :class="`bar-container fixed w-full h-0.5 z-40 ${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'bottom-10' : ''}`">
       <div class="loader-bar absolute h-full filter grayscale bg-gray-800" />
     </div>
     <transition
-      appear
-      appear-active-class="duration-500"
-      enter-active-class="duration-500 in-out"
+      enter-active-class="duration-300 in-out"
       enter-class="opacity-0"
-      appear-class="opacity-0"
+      leave-active-class="duration-300 out-in"
+      leave-to-class="opacity-0"
     >
-      <div :class="`loader-logo fixed z-50 overflow-hidden bg-white ${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'top-0 right-1/2 p-1 transform translate-x-1/2 rounded-b-full' : 'h-24 w-24'}`">
+      <div v-show="loadingLogo" :class="`loader-logo fixed z-50 overflow-hidden bg-white ${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'top-0 right-1/2 p-1 transform translate-x-1/2 rounded-b-full' : 'h-24 w-24'}`">
         <Logo
-          :class="`animate-spin filter opacity-50 contrast-0 brightness-50`"
+          :class="`animate-spin filter opacity-40 contrast-0 brightness-50`"
           :size="`${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'w-12' : 'w-full'}`"
         />
       </div>
@@ -24,15 +23,22 @@
 export default {
   data () {
     return {
-      loading: false
+      loading: false,
+      loadingBar: false,
+      loadingLogo: false,
+      loadingTimer: false
     }
   },
   methods: {
     start () {
       this.loading = true
+      this.loadingBar = true
+      this.loadingLogo = true
     },
     finish () {
-      setTimeout(() => { this.loading = false }, 1000)
+      this.loadingTimer = setTimeout(() => { this.loading = false }, 2000)
+      this.loadingBar = false
+      this.loadingLogo = false
     }
   }
 }

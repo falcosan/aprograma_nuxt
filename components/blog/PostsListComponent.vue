39298015477cbf9780ff1683059ee78a819129a9
falcosan <danielefalche@gmail.com>
2021-05-29 21:37:50 +0200

    .

--- components/blog/PostsListComponent.vue
+++ components/blog/PostsListComponent.vue
@@ -1,10 +1,11 @@
 <template>
-  <ul :class="`post-list w-full grid gap-5 auto-cols-fr ${blok.row_container ? 'lg:grid-cols-container md:auto-rows-max' : 'lg:grid-flow-row lg:auto-rows-fr'}`">
+  <ul :class="`post-list w-full grid gap-5 auto-cols-fr ${blok.row_container && $parent.blok.body.every(item => item.row_container) ? 'lg:grid-cols-container md:auto-rows-max' : 'lg:grid-flow-row lg:auto-rows-fr'}`">
     <PostTeaser
       v-for="post in sortedPosts"
       :key="post._uid"
       :post-link="`blog/${post.slug}`"
       :post-content="post.content"
+      :row-container="$parent.blok.body"
     />
   </ul>
 </template>
--- components/portfolio/ProjectsListComponent.vue
+++ components/portfolio/ProjectsListComponent.vue
@@ -3,12 +3,13 @@
     v-if="sortedProject && $store.state.data.windowWidth >= 1024 && blok.show_slider && !blok.row_container"
     :blok="sortedProject"
   />
-  <ul v-else-if="sortedProject" :class="`project-list grid gap-5 auto-cols-fr ${blok.row_container ? 'lg:grid-cols-container md:auto-rows-max' : 'md:grid-cols-2 lg:grid-cols-3'}`">
+  <ul v-else-if="sortedProject" :class="`project-list grid gap-5 auto-cols-fr ${blok.row_container && $parent.blok.body.every(item => item.row_container) ? 'lg:grid-cols-container md:auto-rows-max' : 'md:grid-cols-2 lg:grid-cols-3'}`">
     <ProjectTeaser
       v-for="project in sortedProject"
       :key="project._uid"
       :project-link="`portfolio/${project.slug}`"
       :project-content="project.content"
+      :row-container="$parent.blok.body"
     />
   </ul>
 </template>

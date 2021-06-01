500b9d1db0701853b77eabd1af4a5e010b9dfe75
falcosan <danielefalche@gmail.com>
2021-06-01 09:34:02 +0200

    ,

--- components/global/ContainerComponent.vue
+++ components/global/ContainerComponent.vue
@@ -27,15 +27,15 @@
       <div class="slider-box overflow-hidden">
         <ul
           v-if="blok.slider_mode === 'slider'"
-          :style="`transform: translateX(${transitionTransform}px); margin-left: ${spaceFix / 2}px; gap: ${spaceFix}px;`"
+          :style="`transform: translateX(${transitionTransform}px);`"
           class="slider relative w-max grid grid-flow-col transition-transform"
         >
           <li
-            v-for="component in blok.body"
+            v-for="(component, index) in blok.body"
             :key="component._uid"
             v-touch:swipe.stop.left="next"
             v-touch:swipe.stop.right="previous"
-            :style="`width: ${slideWidth}px; background-color: ${blok.background_color_component.color};`"
+            :style="`width: ${slideWidth}px; margin-left: ${index === 0 ? '' : spaceFix}px; background-color: ${blok.background_color_component.color};`"
             class="slider-slide slide"
           >
             <component
@@ -216,7 +216,7 @@ export default {
       this.setAutoPlay = 0
     },
     getSliderWidth () {
-      this.slideWidth = this.$el.clientWidth / this.maxElements - this.spaceFix
+      this.slideWidth = this.$el.clientWidth / this.maxElements - (this.spaceFix * (this.maxElements % this.maxElements))
     }
   }
 }

359d9e18f60e6d2ec5a849d932c893a32bd88918
falcosan <danielefalche@gmail.com>
2021-06-02 11:56:25 +0200

    .

--- components/global/ModalComponent.vue
+++ components/global/ModalComponent.vue
@@ -23,7 +23,7 @@
           </header>
           <section
             v-if="hasSlot('body')"
-            :class="`modal-body h-auto px-5 pt-6 lg:pt-9 lg:px-12 ${closeMode ? 'cursor-pointer' : ''}`"
+            :class="`modal-body h-auto px-5 pt-8 lg:pt-9 lg:px-12 ${closeMode ? 'cursor-pointer' : ''}`"
             @click.self.stop="closeMode ? closeModal() : false"
           >
             <transition appear appear-active-class="duration-300" appear-class="opacity-0">
@@ -91,13 +91,13 @@ export default {
 </script>
 <style>
 .body-container{
-    max-height: calc(100vh - 36px);
+  max-height: calc(100vh - 32px);
 }
 .body-container > *:not(hr) {
-  max-height: calc(100vh - 2.6rem);
+  max-height: calc(100vh - 55px);
   align-self: flex-end;
 }
-  .body-container > *:first-child{
-    box-sizing: content-box;
+.body-container > *:first-child{
+  box-sizing: content-box;
 }
 </style>

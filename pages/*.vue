e78f8ad51ecbdf1fe69ab493c0134abaed893fdf
falcosan <danielefalche@gmail.com>
2021-06-03 14:50:00 +0200

    .

--- pages/*.vue
+++ pages/*.vue
@@ -34,17 +34,33 @@ export default {
   },
   async fetch () {
     const slug = (this.$route.path === '/' || this.$route.path === '') ? '/home' : this.$route.path
-    switch (this.$route.name) {
-      case 'portfolio':
-        this.$store.dispatch('list/projects/addProjects', this.$route.name)
-        break
-      case 'blog':
-        this.$store.dispatch('list/posts/addPosts', this.$route.name)
-    }
     const { data } = await this.$storyapi.get(`cdn/stories${slug}`, {
       language: this.$store.state.language.language
     })
     this.story = data.story
+  },
+  head () {
+    return {
+      title: `${this.story.name} - Aprograma`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content () {
+            switch (this.$route.name) {
+              case 'about':
+                return 'About the Aprograma project'
+              case 'portfolio':
+                return 'Blog with last news of Aprograma'
+              case 'contact':
+                return 'Contact the Aprograma'
+              case 'blog':
+                return 'Blog with last news of Aprograma'
+            }
+          }
+        }
+      ]
+    }
   },
   watch: {
     '$store.state.language.language': '$fetch'

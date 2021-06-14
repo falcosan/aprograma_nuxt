e722dc202b46384f3e06a0135a2d374b0a0a9380
falcosan <danielefalche@gmail.com>
2021-06-14 12:24:39 +0200

    .

--- components/layout/MainComponent.vue
+++ components/layout/MainComponent.vue
@@ -1,11 +1,12 @@
 <template>
   <main :class="`main mt-10 ${!$device.isDesktop ? '' : 'md:mt-0 md:py-14'} ${blok.background_media.filename ? '' : 'bg-white'}`">
-    <div :class="`main-wrapper relative max-w-sm xs:max-w-md sm:max-w-lg md:max-w-xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-7xl my-0 mx-auto overflow-x-hidden shadow-md rounded-b-md transition-colors duration-1000 backdrop-filter backdrop-blur ${!$device.isDesktop ? '' : 'md:rounded-b-none md:rounded-t-md'}`" :style="`background-color: ${blok.background_media.filename ? 'transparent' : randomBackgroundColor};`">
-      <div class="main-mask bg-opacity-70 bg-white">
-        <transition enter-active-class="duration-500 in-out" leave-active-class="duration-0 in-out" leave-to-class="opacity-0" enter-class="opacity-0" mode="out-in">
-          <Nuxt />
-        </transition>
+    <div class="main-wrapper relative">
+      <div :class="`main-background absolute max-w-sm xs:max-w-md sm:max-w-lg md:max-w-xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-7xl inset-0 my-0 mx-auto shadow-md rounded-b-md transition-colors duration-1000 backdrop-filter backdrop-blur ${!$device.isDesktop ? '' : 'md:rounded-t-md'}`" :style="`background-color: ${blok.background_media.filename ? 'transparent' : randomBackgroundColor};`">
+        <div class="main-mask h-full w-full bg-opacity-70 bg-white" />
       </div>
+      <transition enter-active-class="duration-500 in-out" leave-active-class="duration-0 in-out" leave-to-class="opacity-0" enter-class="opacity-0" mode="out-in">
+        <Nuxt :class="`relative max-w-sm xs:max-w-md sm:max-w-lg md:max-w-xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-7xl overflow-x-hidden my-0 mx-auto rounded-b-md  ${!$device.isDesktop ? '' : 'md:rounded-t-md'}`" />
+      </transition>
     </div>
     <div v-if="blok.background_media.filename" class="background-media fixed inset-0 -z-10">
       <img
@@ -81,7 +82,7 @@ export default {
 }
 </script>
 <style scoped>
-.main-mask{
+.main-wrapper{
   min-height: calc(100vh - 3.5rem);
 }
 </style>

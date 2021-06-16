d3089d7709def4e64d0c81e0881196c718ae38c1
falcosan <danielefalche@gmail.com>
2021-06-16 14:49:26 +0200

    develop

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="error">
-    <div v-if="error.statusCode === 404" class="h-full flex flex-col justify-center items-center space-y-5 text-center py-10 bg-gray-300">
-      <span class="text-9xl font-bold">{{ error.statusCode }}</span>
+    <div v-if="error.statusCode === 404" class="h-full flex flex-col justify-center items-center space-y-5 text-center p-10 bg-gray-300">
+      <span class="text-7xl font-bold">{{ error.statusCode }}</span>
       <span class="w-max h-max justify-self-center transform rotate-90"> = ( </span>
       <span class="text-2xl"> {{ error.message }}</span>
       <NuxtLink class=" w-max justify-self-center py-3 px-10 filter grayscale bg-gray-600 text-white" to="/">
@@ -9,7 +9,7 @@
       </NuxtLink>
     </div>
     <div v-else class="h-full flex flex-col justify-center items-center space-y-5 text-center py-10 bg-gray-300">
-      <span class="text-9xl font-bold">{{ error.statusCode }}</span>
+      <span class="text-7xl font-bold">{{ error.statusCode }}</span>
       <span class="w-max h-max justify-self-center transform rotate-90">= (</span>
       <span class="text-2xl"> {{ error.message }}</span>
       <NuxtLink class=" w-max justify-self-center py-3 px-10 filter grayscale bg-gray-600 text-white" to="/">

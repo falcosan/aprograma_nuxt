77a1bd530e1494633e2cfd55521e23cab2443e1f
falcosan <danielefalche@gmail.com>
2021-06-16 17:57:54 +0200

    .

--- components/layout/MainComponent.vue
+++ components/layout/MainComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <main :class="`main mt-10 overflow-x-hidden ${!$device.isDesktop ? '' : 'md:mt-0 md:py-14'} ${blok.background_media.filename ? '' : 'bg-white'}`">
     <div :class="`main-wrapper ${$store.state.data.error ? 'main-error' : 'main-regular'} relative`">
-      <div :class="`main-background absolute max-w-sm xs:max-w-md sm:max-w-lg md:max-w-xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-7xl inset-0 my-0 mx-auto shadow-md rounded-b-md transition-colors duration-1000 backdrop-filter backdrop-blur ${!$device.isDesktop ? '' : 'md:rounded-t-md'}`" :style="`background-color: ${blok.background_media.filename ? 'transparent' : randomBackgroundColor};`">
+      <div :class="`main-background absolute max-w-sm xs:max-w-md sm:max-w-lg md:max-w-xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-7xl inset-0 overflow-hidden my-0 mx-auto shadow-md rounded-b-md transition-colors duration-1000 backdrop-filter backdrop-blur ${!$device.isDesktop ? '' : 'md:rounded-t-md'}`" :style="`background-color: ${blok.background_media.filename ? 'transparent' : randomBackgroundColor};`">
         <div class="main-mask h-full w-full bg-opacity-70 bg-white" />
       </div>
       <transition enter-active-class="duration-1000 in-out" enter-class="opacity-0" mode="out-in">

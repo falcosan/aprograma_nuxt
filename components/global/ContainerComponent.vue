8299053da0b313ecd49f996aad5406a03ba71ac7
falcosan <danielefalche@gmail.com>
2021-07-21 19:25:35 +0200

    .

--- components/global/ContainerComponent.vue
+++ components/global/ContainerComponent.vue
@@ -41,7 +41,7 @@
               v-touch:swipe.stop.left="next"
               v-touch:swipe.stop.right="previous"
               :style="`width: ${containerWidth}px; background-color: ${blok.background_color_component.color};`"
-              class="slider-slide slide flex self-start my-0 mx-auto rounded-md"
+              :class="`slider-slide slide flex self-start my-0 mx-auto rounded-md ${sliderMode || carouselMode || containerMode ? '' : 'parent-slide'}`"
             >
               <component
                 :is="component.component"
@@ -70,7 +70,7 @@
               :key="component._uid"
               v-touch:swipe.stop.left="next"
               v-touch:swipe.stop.right="previous"
-              :class="`carousel-slide slide w-full h-full flex row-start-1 row-end-1 col-start-1 col-end-1 rounded-md ${index === currentSlide ? 'show' : 'hidden'}`"
+              :class="`carousel-slide slide w-full h-full flex row-start-1 row-end-1 col-start-1 col-end-1 rounded-md ${index === currentSlide ? 'show' : 'hidden'} ${sliderMode || carouselMode || containerMode ? '' : 'parent-slide'}`"
               :style="`background-color: ${blok.background_color_component.color};`"
             >
               <component
@@ -93,7 +93,7 @@
         v-for="component in elements"
         :key="component._uid"
         :style="`background-color: ${blok.background_color_component.color};`"
-        :class="`${component.name.toLowerCase()}-container w-full grid self-start rounded-md ${component.row_container || $store.state.data.windowWidth < 768 ? '' : 'col-span-full'}`"
+        :class="`${component.name.toLowerCase()}-container w-full grid self-start rounded-md ${sliderMode || carouselMode || containerMode ? '' : 'parent-container'} ${component.row_container || $store.state.data.windowWidth < 768 ? '' : 'col-span-full'}`"
       >
         <component
           :is="component.component"
@@ -329,5 +329,8 @@ export default {
 .show > * {
   position: relative;
   z-index: 10;
+}
+[class*='parent'] > .container-cover > .container-title {
+  font-size: 20px;
 }
 </style>

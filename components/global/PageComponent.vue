972bb03ed9b87710ad8a1dfe292108175b324abd
falcosan <danielefalche@gmail.com>
2021-07-22 23:20:24 +0200

    develop

--- components/global/PageComponent.vue
+++ components/global/PageComponent.vue
@@ -3,7 +3,7 @@
     <h1 v-if="blok.title && blok.show_title" class="page-title mb-5 text-2xl font-extralight">
       {{ blok.title }}
     </h1>
-    <div class="page-components grid gap-5 auto-cols-fr" :style="maxComponents > 1 ? `grid-template-columns: repeat(${$rangeItems(maxComponents, 3)}, 1fr);` : ''">
+    <div class="page-components grid gap-x-5 gap-y-10 auto-cols-fr" :style="maxComponents > 1 ? `grid-template-columns: repeat(${$rangeItems(maxComponents, 3)}, 1fr);` : ''">
       <div
         v-for="component in blok.body"
         :key="component._uid"

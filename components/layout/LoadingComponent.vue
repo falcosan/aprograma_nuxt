1fac3c674f714c95e63eb9070d17c38397f0dafb
falcosan <danielefalche@gmail.com>
2021-07-26 10:03:18 +0200

    develop

--- components/layout/LoadingComponent.vue
+++ components/layout/LoadingComponent.vue
@@ -1,33 +1,27 @@
 <template>
   <div v-if="loading" class="loader">
-    <div v-if="!$device.isDesktop && $store.state.data.windowWidth < 768" class="loader-bar">
-      <transition
-        enter-active-class="duration-100 in-out"
-        enter-class="opacity-0"
-        leave-active-class="duration-100 out-in"
-        leave-to-class="opacity-0"
-      >
-        <div v-show="loadingItem" class="bar-container fixed h-0.5 w-full bottom-12 z-50 overflow-hidden bg-gray-200">
-          <div class="loading-bar absolute -left-1/2 h-full w-1/2 filter grayscale bg-gray-800" />
-        </div>
-      </transition>
-    </div>
-    <div v-else class="loader-logo">
-      <div v-show="loadingItem" class="logo-container fixed h-20 w-20 flex items-center justify-center top-0 z-50 overflow-hidden rounded-br-lg bg-white">
+    <transition
+      enter-active-class="duration-300 in-out"
+      enter-class="opacity-0"
+      leave-active-class="duration-700 out-in"
+      leave-to-class="opacity-0"
+    >
+      <div v-show="loadingLogo" :class="`loader-logo fixed flex items-center justify-center top-0 z-50 overflow-hidden bg-white ${!$device.isDesktop || $store.state.data.windowWidth < 768 ? '-top-0.5 right-1/2 p-1 transform translate-x-1/2 rounded-b-full' : 'h-20 w-20 rounded-br-lg'}`">
         <Logo
-          class="loading-logo filter opacity-50 contrast-0 brightness-50"
-          size="w-16"
+          class="logo-spin filter opacity-50 contrast-0 brightness-50"
+          :size="`${!$device.isDesktop || $store.state.data.windowWidth < 768 ? 'w-11' : 'w-16'}`"
         />
       </div>
-    </div>
+    </transition>
   </div>
 </template>
+
 <script>
 export default {
   data () {
     return {
       loading: false,
-      loadingItem: false,
+      loadingLogo: false,
       loadingTimer: 0
     }
   },
@@ -35,34 +29,22 @@ export default {
     start () {
       clearTimeout(this.loadingTimer)
       this.loading = true
-      this.loadingItem = true
+      this.loadingLogo = true
     },
     finish () {
       this.loadingTimer = setTimeout(() => { this.loading = false }, 1000)
-      this.loadingItem = false
+      this.loadingLogo = false
     }
   }
 }
 </script>
 
 <style scoped>
-.loading-logo{
-  filter: blur(0.5px);
+.logo-spin{
+  filter: blur(1px);
   animation: spinning 0.5s linear infinite;
 }
 
-.loading-bar {
-  animation: loading 1.5s ease-in 0.5s infinite;
-}
-
-@keyframes loading {
- 0% {
-  transform:translateX(0)
- }
- to {
-  transform:translateX(400%)
- }
-}
 @keyframes spinning {
   from {
     transform: rotate(0deg);

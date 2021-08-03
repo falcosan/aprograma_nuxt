de9cccd082a3b163f625f0c41a48336ec7135119
falcosan <danielefalche@gmail.com>
2021-08-03 09:24:28 +0200

    .

--- components/global/TextContentComponent.vue
+++ components/global/TextContentComponent.vue
@@ -6,7 +6,7 @@
     :class="`text-container w-full max-w-full h-full rounded ${customClass('index', blok.name === 'text_phrase', 'transition-colors duration-700 ease-in-out')}`"
   >
     <span
-      :class="`text-content h-full max-w-none block p-5 lg:p-10 rounded prose-sm lg:prose-lg break-all ss:break-normal`"
+      :class="`text-content h-full max-w-none block p-5 lg:p-8 rounded prose-sm lg:prose-lg break-all ss:break-normal`"
       v-html="$md.render(blok.text)"
     />
   </div>

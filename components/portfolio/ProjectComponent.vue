ff32d2196e576c1029ee0d10399d109086608367
falcosan <danielefalche@gmail.com>
2021-08-03 14:54:30 +0200

    .

--- components/portfolio/ProjectComponent.vue
+++ components/portfolio/ProjectComponent.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="project grid gap-5 p-5">
-    <h1 class="project-title flex justify-center lg:justify-start items-center col-start-1 col-end-4 lg:col-end-auto lg:px-5 text-2xl xs:text-3xl text-center lg:text-left font-extralight break-words">
+  <div class="project grid gap-5 auto-cols-fr p-5">
+    <h1 class="project-title flex justify-center lg:justify-start items-center col-start-1 col-end-4 xl:col-end-3 lg:px-5 text-2xl xs:text-3xl text-center xl:text-left font-extralight break-words">
       {{ blok.title }}
     </h1>
+    <Icon
+      v-if="$store.state.data.windowWidth >= 1280"
+      arrow
+      tag="button"
+      :style="`background-color: ${blok.project_background_color.color}; color: ${blok.project_text_color.color};`"
+      class="project-back h-10 w-10 self-end justify-self-end rounded hover:shadow transition-shadow duration-100"
+      size="p-3 w-10"
+      @click.native="goBack()"
+    />
     <div class="project-intro grid gap-5 auto-cols-fr col-start-1 col-end-4">
       <Modal
         v-if="blok.image.filename"
@@ -32,16 +41,26 @@
           >
         </template>
       </Modal>
-      <component
-        :is="blok.url_project ? 'a' : 'span'"
-        v-if="$store.state.data.windowWidth >= 1024"
-        :class="`project-url xl:row-start-2 xl:row-end-2 xl:col-start-3 xl:col-end-3 text-center truncate ${blok.url_project ? 'font-medium' : 'italic'}`"
-        :href="blok.url_project ? blok.url_project : ''"
-        :target="blok.url_project ? '_blank' : ''"
-        :rel="blok.url_project ? 'noopener noreferrer' : ''"
-      >
-        {{ blok.url_project ? `${$languageCase('link to', 'enlace por', 'link per')} ${blok.title}` : $languageCase('private project', 'proyecto privado', 'progetto privato') }}
-      </component>
+      <div class="project-action min-w-0 flex items-center justify-between xl:row-start-2 xl:row-end-2 xl:col-start-3 xl:col-end-3">
+        <component
+          :is="blok.url_project ? 'a' : 'span'"
+          :class="`project-url mr-5 ss:my-0 ss:mx-auto ss:pl-10 xl:pl-0 text-center text-sm sm:text-base truncate ${blok.url_project ? 'font-medium' : 'italic'}`"
+          :href="blok.url_project ? blok.url_project : false"
+          :target="blok.url_project ? '_blank' : false"
+          :rel="blok.url_project ? 'noopener noreferrer' : false"
+        >
+          {{ blok.url_project ? `${$languageCase('link to', 'enlace por', 'link per')} ${blok.title}` : $languageCase('private project', 'proyecto privado', 'progetto privato') }}
+        </component>
+        <Icon
+          v-if="$store.state.data.windowWidth < 1280"
+          arrow
+          tag="button"
+          :style="`background-color: ${blok.project_background_color.color}; color: ${blok.project_text_color.color};`"
+          class="project-back w-10 rounded hover:shadow transition-shadow duration-100"
+          size="p-3 w-10"
+          @click.native="goBack()"
+        />
+      </div>
       <span
         :style="`background-color: ${blok.project_background_color.color}; color: ${blok.project_text_color.color};`"
         class="intro-text p-5 rounded"
@@ -68,26 +87,6 @@
         :key="description._uid"
         :inline-text-style="`background-color: ${blok.project_background_color.color}; color: ${blok.project_text_color.color};`"
         :blok="description"
-      />
-    </div>
-    <div class="project-action w-auto min-w-0 flex lg:block self-center lg:justify-self-end items-center col-start-1 col-end-4 lg:row-end-1 lg:col-start-3 lg:col-end-4">
-      <component
-        :is="blok.url_project ? 'a' : 'span'"
-        v-if="$store.state.data.windowWidth < 1024"
-        :class="`project-url w-full rounded mr-5 text-center truncate ${blok.url_project ? 'font-medium' : 'italic'}`"
-        :href="blok.url_project ? blok.url_project : false"
-        :target="blok.url_project ? '_blank' : false"
-        :rel="blok.url_project ? 'noopener noreferrer' : false"
-      >
-        {{ blok.url_project ? `${$languageCase('link to', 'enlace por', 'link per')} ${blok.title}` : $languageCase('private project', 'proyecto privado', 'progetto privato') }}
-      </component>
-      <Icon
-        arrow
-        tag="button"
-        :style="`background-color: ${blok.project_background_color.color}; color: ${blok.project_text_color.color};`"
-        class="project-back w-10 rounded hover:shadow transition-shadow duration-100"
-        size="p-3 w-10"
-        @click.native="goBack()"
       />
     </div>
   </div>

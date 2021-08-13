2dd38e8cf7c94732e20a315504816f203a911f1b
falcosan <danielefalche@gmail.com>
2021-08-13 16:23:56 +0200

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -105,7 +105,10 @@ export default {
     linkify: true,
     runtime: true,
     typographer: true,
-    breaks: true
+    breaks: true,
+    use: [
+      'markdown-it-attrs'
+    ]
   },
 
   sitemap: {

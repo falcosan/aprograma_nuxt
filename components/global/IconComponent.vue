740cf9c02f3470a5e143980a547ee892ec85eefb
falcosan <danielefalche@gmail.com>
2021-08-20 15:01:56 +0200

    .

--- components/global/IconComponent.vue
+++ components/global/IconComponent.vue
@@ -9,6 +9,8 @@
         .split(/[\\/]/)
         .pop()
         .replace(/\.[^/.]+$/, '')}-icon mx-auto my-0 fill-current object-contain object-center pointer-events-none`"
+      :style="`width: ${blok && blok.size ? `${blok.size}`: 'auto'}; height: ${blok && blok.size ? `${blok.size}`: 'auto'}`"
+      :title="blok && blok.title && !blok.show_title || tooltip ? blok ? blok.title : tooltip : ''"
       :src="blok.icon_image.filename"
       alt=""
       :height="blok.size ? blok.size : 'auto'"
--- components/global/LinkComponent.vue
+++ components/global/LinkComponent.vue
@@ -19,6 +19,7 @@
       :tag="blok.body[0].tag"
       :blok="blok.body[0]"
       :slider-mode="sliderMode"
+      :tooltip="blok.title && blok.icon_item ? false : undefined"
       :carousel-mode="carouselMode"
       :container-mode="containerMode"
     />

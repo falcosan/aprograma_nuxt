c0bd6b324cf6e245e6adf78aa20fa3fc1052f998
falcosan <danielefalche@gmail.com>
2021-08-20 04:09:14 +0200

    develop

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap' }
     ]
   },
-  loading: false,
+  loading: context => !!context.$device.isMobile,
   plugins: [
     '~/plugins/injects',
     '~/plugins/components',

125a396d7f341c4607378a08f8e5847bd45ac1d2
falcosan <danielefalche@gmail.com>
2021-08-23 04:35:14 +0200

    develop

--- nuxt.config.js
+++ nuxt.config.js
@@ -260,7 +260,7 @@ export default {
         collapseBooleanAttributes: true,
         decodeEntities: true,
         minifyCSS: false,
-        minifyJS: true,
+        minifyJS: false,
         processConditionalComments: true,
         removeEmptyAttributes: true,
         removeRedundantAttributes: true,

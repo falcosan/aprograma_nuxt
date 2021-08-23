751168e591feaf69ecd1df3a38c9223043e64833
falcosan <danielefalche@gmail.com>
2021-08-23 18:15:57 +0200

    .

--- components/global/DetailComponent.vue
+++ components/global/DetailComponent.vue
@@ -122,7 +122,7 @@
         </li>
       </ul>
       <div :class="`text-container w-full max-w-full flex flex-col self-start rounded ${!blok.remove_space ? 'p-5' : ''}`" :style="inlineTextStyle ? inlineTextStyle : `background-color: ${blok.background_color.color}; color: ${blok.text_color.color};`">
-        <div class="detail-text block max-w-none rounded prose-sm lg:prose-lg" v-html="$md.render(blok.text)" />
+        <div class="detail-text markdown block max-w-none rounded prose-sm lg:prose-lg" v-html="$md.render(blok.text)" />
       </div>
     </div>
   </div>

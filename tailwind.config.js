607b055162805450184da62e1d73c9c536fb4269
falcosan <danielefalche@gmail.com>
2021-08-31 22:54:58 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,12 +51,12 @@ module.exports = {
         'fit-huge': 'repeat(auto-fit, minmax(400px, 1fr))',
         'fit-big': 'repeat(auto-fit, minmax(315px, 1fr))',
         'fit-medium': 'repeat(auto-fit, minmax(257px, 1fr))',
-        'fit-small': 'repeat(auto-fit, minmax(159px, 1fr))',
+        'fit-small': 'repeat(auto-fit, minmax(157.5px, 1fr))',
         'fit-tiny': 'repeat(auto-fit, minmax(130px, 1fr))',
         'fill-huge': 'repeat(auto-fill, minmax(400px, 1fr))',
         'fill-big': 'repeat(auto-fill, minmax(315px, 1fr))',
         'fill-medium': 'repeat(auto-fill, minmax(257px, 1fr))',
-        'fill-small': 'repeat(auto-fill, minmax(159px, 1fr))',
+        'fill-small': 'repeat(auto-fill, minmax(157.5px, 1fr))',
         'fill-tiny': 'repeat(auto-fill, minmax(130px, 1fr))'
       },
       opacity: {

312aea468427cc3aae8d3142b0e1a480952ee96b
falcosan <danielefalche@gmail.com>
2021-08-31 14:56:06 +0200

    .

--- components/portfolio/ProjectComponent.vue
+++ components/portfolio/ProjectComponent.vue
@@ -42,7 +42,7 @@
         modal-style="bg-gray-200 bg-opacity-90"
       >
         <template #activator="action">
-          <div class="image-container w-full aspect-w-4 aspect-h-3 my-0 mx-auto cursor-pointer" @click="action.open()">
+          <div class="image-container w-full aspect-w-11 aspect-h-10 my-0 mx-auto cursor-pointer" @click="action.open()">
             <NuxtImg
               loading="lazy"
               format="webp"

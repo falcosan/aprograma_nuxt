c50c4adbf982db75607f6f8f8fbd83e05c18a90d
falcosan <danielefalche@gmail.com>
2021-08-31 22:59:14 +0200

    .

--- components/blog/PostsListComponent.vue
+++ components/blog/PostsListComponent.vue
@@ -21,7 +21,7 @@
             :class="`category-container h-full flex justify-between overflow-hidden rounded cursor-pointer select-none transition-all filter grayscale bg-gray-600 text-white ${searchCategory.includes(filter) ? 'bg-opacity-70' : !$device.isDesktop ? '' : 'hover:bg-gray-700'}`"
             @click="filterSearch(filter)"
           >
-            <Input :class="`category-input w-full text-left rounded italic truncate transition-all ${searchCategory.includes(filter) ? 'filter grayscale bg-gray-500' : 'bg-transparent'}`" type="button" :text="filter" />
+            <Input :class="`category-input w-full text-left rounded pointer-events-none italic truncate transition-all ${searchCategory.includes(filter) ? 'filter grayscale bg-gray-500' : 'bg-transparent'}`" type="button" :text="filter" />
             <Icon close tag="span" size="w-2 h-2" :class="`px-5 pointer-events-none transition ${searchCategory.includes(filter) ? '' : 'transform rotate-45'}`" />
           </li>
           <li class="reset-container h-full overflow-hidden col-start-1 col-end-1 row-start-1 row-end-1 rounded cursor-pointer select-none" @click="searchCategory = []">

b373c477c17e8e5ce30de3dc6d51a3008d652735
falcosan <danielefalche@gmail.com>
2021-09-19 20:29:14 +0200

    develop

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,7 +99,7 @@ export default {
     routes (callback) {
       const exclude = ['home', 'layout']
       const routes = []
-      axios(`https://api.storyblok.com/v2/cdn/links?token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP`).then((res) => {
+      axios(`https://api.storyblok.com/v2/cdn/links?token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP&version=published`).then((res) => {
         Object.keys(res.data.links).forEach((key) => {
           if (!exclude.includes(res.data.links[key].slug)) {
             routes.push('/' + res.data.links[key].slug)
@@ -178,7 +178,7 @@ export default {
     hostname: 'https://aprograma.co',
     trailingSlash: true,
     routes: async () => {
-      const { data } = await axios(`https://api.storyblok.com/v2/cdn/links?token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP`)
+      const { data } = await axios(`https://api.storyblok.com/v2/cdn/links?token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP&version=published`)
       const exclude = ['home', 'layout']
       const include = Object.values(data.links).map(link => !exclude.includes(link.slug) ? link.slug : '')
       return include.filter(Boolean)
@@ -204,7 +204,7 @@ export default {
           email: 'danielefalche@gmail.com',
           link: 'https://aprograma.co'
         })
-        const dataEng = await axios(`https://api.storyblok.com/v2/cdn/stories?starts_with=blog&token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP`)
+        const dataEng = await axios(`https://api.storyblok.com/v2/cdn/stories?starts_with=blog&token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP&version=published`)
         const dataFiltered = dataLang => dataLang.data.stories.filter(filteredPost => filteredPost.name.toLowerCase() !== 'blog')
         dataFiltered(dataEng).forEach((post) => {
           feed.addItem({
@@ -235,7 +235,7 @@ export default {
           email: 'danielefalche@gmail.com',
           link: 'https://aprograma.co'
         })
-        const dataEs = await axios(`https://api.storyblok.com/v2/cdn/stories?starts_with=es/blog&token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP`)
+        const dataEs = await axios(`https://api.storyblok.com/v2/cdn/stories?starts_with=es/blog&token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP&version=published`)
         const dataFiltered = dataLang => dataLang.data.stories.filter(filteredPost => filteredPost.name.toLowerCase() !== 'blog')
         dataFiltered(dataEs).forEach((post) => {
           feed.addItem({
@@ -266,7 +266,7 @@ export default {
           email: 'danielefalche@gmail.com',
           link: 'https://aprograma.co'
         })
-        const dataIt = await axios(`https://api.storyblok.com/v2/cdn/stories?starts_with=it/blog&token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP`)
+        const dataIt = await axios(`https://api.storyblok.com/v2/cdn/stories?starts_with=it/blog&token=${process.env.NUXT_ENV_PREVIEW_TOKEN}&cv=CURRENT_TIMESTAMP&version=published`)
         const dataFiltered = dataLang => dataLang.data.stories.filter(filteredPost => filteredPost.name.toLowerCase() !== 'blog')
         dataFiltered(dataIt).forEach((post) => {
           feed.addItem({
